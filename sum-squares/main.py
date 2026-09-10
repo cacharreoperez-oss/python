caa6243c4f2dd1a1aacf00d402ddbd3cfd5e079c
cacharreo <cacharreoperez@gmail.com>
2026-09-10 09:44:05 +0200

    ActualizaciÃ³n automÃ¡tica 2026-09-10 09:44

---
+++ sum-squares/main.py
@@ -0,0 +1,13 @@
+def f(x, y):
+    # TODO: Implement the function to calculate the sum of squares
+    total = 0
+   
+    total= x**2 + y**2
+    return total
+
+
+# DO NOT TOUCH THE CODE BELOW
+if __name__ == '__main__':
+    import vendor
+
+    vendor.launch(f)
