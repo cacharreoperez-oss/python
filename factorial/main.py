b6182fbdf3c4ab207f856f832db30848f1a653c3
cacharreo <cacharreoperez@gmail.com>
2026-09-10 10:29:05 +0200

    ActualizaciÃ³n automÃ¡tica 2026-09-10 10:29

--- factorial/main.py
+++ factorial/main.py
@@ -1,10 +1,15 @@
 def factorial(n):
     if n < 0:
         return None
+
     if n == 0:
         return 1
-    else:
-        return n * factorial(n-1)
+
+    resultado = 1
+    for numero in range(1, n + 1):
+        resultado *= numero
+
+    return resultado
 
 
 # DO NOT TOUCH THE CODE BELOW
