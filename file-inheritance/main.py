1dda107c24ca819c78b830cbc0aa986e318ecb39
cacharreo <cacharreoperez@gmail.com>
2026-09-14 12:29:05 +0200

    ActualizaciÃ³n automÃ¡tica 2026-09-14 12:29

--- file-inheritance/main.py
+++ file-inheritance/main.py
@@ -1,1 +1,16 @@
-# TODO
+class File:
+
+    def __init__(self,path:str):
+        self.path =path
+        self.content = []
+        
+    def add_contentb(self,content:list):
+        self.content=content
+    @property
+    def size(self):
+        pass
+
+    @property
+    def info(self):
+        pass
+
