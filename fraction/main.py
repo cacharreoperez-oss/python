5adbabc2af04d821906aa2da2f07459f95c028f3
cacharreo <cacharreoperez@gmail.com>
2026-09-14 10:29:05 +0200

    ActualizaciÃ³n automÃ¡tica 2026-09-14 10:29

--- fraction/main.py
+++ fraction/main.py
@@ -1,3 +1,8 @@
 from __future__ import annotations
 
-# TODO
+class Fraction:
+    def __init__(self,num:int , den:int):
+        self.num = num
+        self.den= den
+    def __str__(self)
+        print (f"{self.num} / {self.den} ")
